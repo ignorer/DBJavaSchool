78835e6d1edeea480976bf3e68bce32783acbd33
ivaserg <ivanov.svv@gmail.com>
2017-09-07 15:35:51 +0300

    corrections

--- src/main/java/com/db/javaschool/client/OutputMain.java
+++ src/main/java/com/db/javaschool/client/OutputMain.java
@@ -1,5 +1,6 @@
 package com.db.javaschool.client;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -8,34 +9,56 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class OutputMain {
-    private Socket socket;
+    private String HOSTNAME = "127.0.0.1";
+    private int PORT = 6666;
 
-    public OutputMain(Socket socket) {
-        this.socket = socket;
+    public OutputMain() {
+    }
+
+    public OutputMain(String HOSTNAME, int PORT) {
+        this.HOSTNAME = HOSTNAME;
+        this.PORT = PORT;
+    }
+
+    private Socket openConnection() throws IOException {
+        return new Socket(HOSTNAME, PORT);
+    }
+
+    private static BufferedReader getBufferedReader(Socket socket) throws IOException {
+        return new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+    }
+
+    public void readMessages(BufferedReader bufferedReader) throws IOException {
+            StringBuilder stringBuilder = new StringBuilder();
+            String inputStr;
+            while ((inputStr = bufferedReader.readLine()) != null) {
+                stringBuilder.append(inputStr);
+            }
+            JSONObject json = new JSONObject(stringBuilder.toString());
+            String jsonMessageType = (String) json.get("type");
+            if (json.get("type") == "snd" ) {
+                String message = (String) json.get("msg");
+                outputMessages(message);
+            }
+
+    }
+
+
+    public void outputMessages(String message) {
+        System.out.println(message);
     }
 
     public static void main(String[] args) throws IOException {
 
-        OutputMain outputMain = new OutputMain(new Socket("127.0.0.1", 6666));
-        outputMain.displayMessages();
+        if (args.length==0) {
+            System.out.println("Invalid number of arguments");
+            System.exit(1);
+        }
 
-    }
-
-    public void displayMessages() throws IOException {
-        BufferedReader streamReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-
+        OutputMain outputMain = new OutputMain();
+        BufferedReader bufferedReader = getBufferedReader(outputMain.openConnection());
         while (true) {
-            StringBuilder responseStrBuilder = new StringBuilder();
-            String inputStr;
-            while ((inputStr = streamReader.readLine()) != null) {
-                responseStrBuilder.append(inputStr);
-            }
-            JSONObject json = new JSONObject(responseStrBuilder.toString());
-            String jsonMessageType = (String) json.get("type");
-            if (json.get("type") == null ) {
-                System.out.println();
-            }
-
+            outputMain.readMessages(bufferedReader);
         }
 
     }
