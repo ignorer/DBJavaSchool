76624f5f398e71eeb58356e246fd46a40b40ae0f
imbananko <imbananko@gmail.com>
2017-09-07 12:45:57 +0300

    added first server test

--- src/test/com/db/javaschool/server/ServerTest.java
+++ src/test/com/db/javaschool/server/ServerTest.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import com.db.javaschool.server.MessagePool;
 
+import java.io.IOException;
+import java.net.Socket;
+
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class ServerTest {
@@ -15,13 +19,27 @@ public class ServerTest {
     }
     @Test
     public void shouldDumpToFileWhenPoolIsGoingToOverflow() {
-        MessagePool real = new MessagePool();
-        MessagePool spy = Mockito.spy(real);
+        MessagePool spy = Mockito.spy(new MessagePool());
 
         for (int i = 0; i < 1000; i++) {
-            real.addMessage(i + "");
+            spy.addMessage(i + "");
         }
 
         verify(spy).dumpToFile();
+    }
+
+    @Test
+    public void test() throws IOException {
+        Context mockedContext = mock(Context.class);
+        Socket mockedSocket = mock(Socket.class);
+
+
+        mockedContext.add("test", mockedSocket);
+        mockedContext.sendAll("message");
+
+
+        verify(mockedSocket).getOutputStream();
+
+
     }
 }
