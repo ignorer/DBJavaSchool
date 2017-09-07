26d3b107138b98d8f7345e57a23f8c2c2bfb1363
ivaserg <ivanov.svv@gmail.com>
2017-09-07 12:23:33 +0300

    minor correcttions

---
+++ src/main/java/client/OutputMain.java
@@ -0,0 +1,44 @@
+package client;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class OutputMain {
+    private Socket socket;
+
+    public OutputMain(Socket socket) {
+        this.socket = socket;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        OutputMain outputMain = new OutputMain(new Socket("127.0.0.1", 6666));
+        outputMain.displayMessages();
+
+    }
+
+    public void displayMessages() throws IOException {
+        BufferedReader streamReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+
+        while (true) {
+            StringBuilder responseStrBuilder = new StringBuilder();
+            String inputStr;
+            while ((inputStr = streamReader.readLine()) != null) {
+                responseStrBuilder.append(inputStr);
+            }
+            JSONObject json = new JSONObject(responseStrBuilder.toString());
+            String jsonMessageType = (String) json.get("type");
+            if (json.get("type") == null ) {
+                System.out.println();
+            }
+
+        }
+
+    }
+
+
+}
